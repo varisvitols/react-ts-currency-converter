304fd2b1858bfc43d4c1daa063c8d104c6e8a48f
Varis Vitols <varisvitols@gmail.com>
2022-10-02 19:24:05 +0300

    Add React Router, create routes and corresponding components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./routes/home/home.component";
+import Admin from "./routes/admin/admin.component";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Routes>
+      <Route path="/">
+        <Route index element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
