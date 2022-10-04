8a0e916abbad80e896cb939907b57b4a0bdbf642
Varis Vitols <varisvitols@gmail.com>
2022-10-04 08:37:33 +0300

    Implement CurrencyInput component. Let it pass selected value up to the parent component

--- src/components/form/currency-input/currency-input.component.tsx
+++ src/components/form/currency-input/currency-input.component.tsx
@@ -1,12 +1,38 @@
+import React from "react";
+import { useExchangeRates } from "../../../contexts/exchange-rates.context";
 import "./currency-input.styles.scss";
 
-function CurrencyInput() {
+type CurrencyInputProps = {
+  inputDirection: string;
+  handleCurrencyInputChange: (value: {
+    direction: string;
+    value: string;
+  }) => void;
+};
+
+function CurrencyInput({
+  inputDirection,
+  handleCurrencyInputChange,
+}: CurrencyInputProps) {
+  const { exchangeRates } = useExchangeRates();
+
+  const currencyChangeHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    handleCurrencyInputChange({
+      direction: inputDirection,
+      value: e.target.value,
+    });
+  };
+
   return (
-    <select className="currency-input">
-      <option value="cat">Cat</option>
-      <option value="dog">Dog</option>
-      <option value="rabbit">Rabbit</option>
-      <option value="bear">Bear</option>
+    <select className="currency-input" onChange={currencyChangeHandler}>
+      <option></option>
+      {exchangeRates.map((item) => {
+        return (
+          <option key={item.currency} value={item.currency}>
+            {item.currency}
+          </option>
+        );
+      })}
     </select>
   );
 }
